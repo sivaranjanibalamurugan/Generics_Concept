bb22726a1e702065c142b0a70e049dbbde47caad
unknown <sivaranjanib99@gmail.com>
2021-07-09 16:27:49 +0530

    [siva] finding max of three using testing

--- TestProject1/MaxOfThree.cs
+++ TestProject1/MaxOfThree.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace TestProject1
+namespace UnitTest
 {
     class MaxOfThree
     {
+        public int FindMax(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+
+            }
+            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+
+                return num3;
+            }
+            else
+            {
+                Console.WriteLine("All are having same number");
+                return 0;
+            }
+
+        }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,13 +1,36 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-namespace TestProject1
+namespace UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
+        MaxOfThree maxOfThree;
+
+        public void UnitTest()
+        {
+            maxOfThree = new MaxOfThree();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            int num1, num2, num3, actual, expected;
+            num1 = 45; num2 = 35; num3 = 25;
+            actual = maxOfThree.FindMax(num1, num2, num3);
+            expected = num1;
+            expected.CompareTo(actual);
+
+            num1 = 35; num2 = 45; num3 = 25;
+            actual = maxOfThree.FindMax(num1, num2, num3);
+            expected = num2;
+            expected.CompareTo(actual);
+
+            num1 = 25; num2 = 35; num3 = 45;
+            actual = maxOfThree.FindMax(num1, num2, num3);
+            expected = num1;
+            expected.CompareTo(actual);
         }
     }
 }
