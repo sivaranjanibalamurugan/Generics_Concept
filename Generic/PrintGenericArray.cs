e6b70dc0bc933179775774882a47cb1778646a52
unknown <sivaranjanib99@gmail.com>
2021-07-09 18:17:09 +0530

    [siva] printing the generic method

--- Generic/PrintGenericArray.cs
+++ Generic/PrintGenericArray.cs
@@ -6,22 +6,27 @@ using System.Threading.Tasks;
 
 namespace PrintGenericArray
 {
-    class PrintArray<T>
+    class PrintArray
     {
-        public T[] inputArray;
-        public PrintArray(T[] inputArray)
-        {
-            this.inputArray = inputArray;
-        }
         //Printing the array using generic type T
-        public void ToPrintArray()
+        public static void ToPrintArray<T>(T[] inputArray)
         {
             Console.WriteLine("Printing Array");
-            foreach (var i in this.inputArray)
+            foreach (var i in inputArray)
             {
                 Console.WriteLine(i);
             }
         }
 
+
+        public static void ReadInput()
+        {
+            int[] intArray = { 10, 5, 100, 85, 96, 2, 1 };
+            double[] doubleArray = { 1.1, 1.3, 5.6, 5.36, 6.2, 80.5, 1 };
+            char[] charArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+            ToPrintArray(intArray);
+            ToPrintArray(doubleArray);
+            ToPrintArray(charArray);
+        }
     }
 }
--- Generic/Program.cs
+++ Generic/Program.cs
@@ -4,16 +4,11 @@ namespace PrintGenericArray
 {
     class Program
     {
+      
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to generic programming !!");
             Console.WriteLine("Printing array of different data type");
-            int[] intArray = { 10, 5, 100, 85, 96, 2, 1 };
-            double[] doubleArray = { 1.1, 1.3, 5.6, 5.36, 6.2, 80.5, 1 };
-            char[] charArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
-            new PrintArray<int>(intArray).ToPrintArray();
-            new PrintArray<double>(doubleArray).ToPrintArray();
-            new PrintArray<char>(charArray).ToPrintArray();
+            PrintArray.ReadInput();
             Console.Read();
         }
     }
