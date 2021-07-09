116ca62fd003b3d6ea9fd2e33c6a953b0098ae84
unknown <sivaranjanib99@gmail.com>
2021-07-09 17:27:45 +0530

    [siva] Printing the genetic value

---
+++ Generic/PrintGenericArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintGenericArray
+{
+    class PrintArray<T>
+    {
+        public T[] inputArray;
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        //Printing the array using generic type T
+        public void ToPrintArray()
+        {
+            Console.WriteLine("Printing Array");
+            foreach (var i in this.inputArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
