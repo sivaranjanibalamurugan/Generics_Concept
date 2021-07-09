5c99fec97aaf11559f4df2820a3cd624f47e3415
unknown <sivaranjanib99@gmail.com>
2021-07-09 16:36:34 +0530

    [siva] finding the Max of three string using testing

--- TestProject1/MaxOfThree.cs
+++ TestProject1/MaxOfThree.cs
@@ -6,26 +6,26 @@ namespace UnitTest
 {
     class MaxOfThree
     {
-        public int FindMax(int num1, int num2, int num3)
+        public string FindMax(string str1, string str2, string str3)
         {
-            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            if (str1.CompareTo(str2) > 0 && str1.CompareTo(str3) > 0)
             {
-                return num1;
+                return str1;
 
             }
-            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            else if (str2.CompareTo(str1) > 0 && str2.CompareTo(str3) > 0)
             {
-                return num2;
+                return str2;
             }
-            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            else if (str3.CompareTo(str1) > 0 && str3.CompareTo(str2) > 0)
             {
 
-                return num3;
+                return str3;
             }
             else
             {
-                Console.WriteLine("All are having same number");
-                return 0;
+                Console.WriteLine("All are same");
+                return " ";
             }
 
         }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -16,21 +16,25 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
-            int num1, num2, num3, actual, expected;
-            num1 = 45; num2 = 35; num3 = 25;
-            actual = maxOfThree.FindMax(num1, num2, num3);
-            expected = num1;
+            string str1, str2, str3, actual, expected;
+            str1 = "Apple"; str2 = "Peach"; str3 = "Banana";
+            actual = maxOfThree.FindMax(str1, str2, str3);
+            expected = str1;
             expected.CompareTo(actual);
 
-            num1 = 35; num2 = 45; num3 = 25;
-            actual = maxOfThree.FindMax(num1, num2, num3);
-            expected = num2;
+            str1 = "Peach"; str2 = "Apple"; str3 = "Banana";
+            actual = maxOfThree.FindMax(str1, str2, str3);
+            expected = str1;
             expected.CompareTo(actual);
 
-            num1 = 25; num2 = 35; num3 = 45;
-            actual = maxOfThree.FindMax(num1, num2, num3);
-            expected = num1;
+            str1 = "Banana"; str2 = "Peach"; str3 = "Apple";
+            actual = maxOfThree.FindMax(str1, str2, str3);
+            expected = str1;
             expected.CompareTo(actual);
+
+
+
+
         }
     }
 }
